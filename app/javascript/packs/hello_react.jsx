2fd5f09c7fbbdc01b55e49b42fbbf2380b69d3ec
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-01 21:26:16 +0200

    update hello_react file

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -2,25 +2,49 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+// const Hello = props => (
+//   <div>Helloooooooooooooooooooooooooooooooooooooo {props.name}!</div>
+// )
 
-Hello.defaultProps = {
-  name: 'David'
-}
+// Hello.defaultProps = {
+//   name: 'David'
+// }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
+// Hello.propTypes = {
+//   name: PropTypes.string
+// }
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(
+//     <Hello name="React" />,
+//     document.body.appendChild(document.createElement('div')),
+//   )
+// })
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Paintings from "../src/pages/Paintings";
+import Painting from "../src/pages/Painting";
+import Biography from "../src/pages/Biography";
+import Contact from "../src/pages/Contact";
+
+document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/biography" element={<Biography />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/tableaux" element={<Paintings />} />
+        <Route path="/tableaux/:id" element={<Painting />} />
+      </Routes>
+    </Router>,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
