d701d17258b819ef65ec958a2bc6568de34f7cca
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-01 10:27:47 +0200

    create seeds parsing CSV file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Painting.destroy_all
+
+puts "All previous paintings deleted"
+
+
+CSV.foreach(Rails.root.join('lib/seeds/paintings.csv'), headers: true, col_sep: ';') do |row|
+
+  Painting.find_or_create_by(
+    {
+      name: row[0],
+      description: row[1],
+      format: row[2],
+      price: row[3],
+      year: row[4]
+    }
+  )
+
+end
+
+puts "All paintings saved"
