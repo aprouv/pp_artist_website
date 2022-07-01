56376b472cbdecd6838a2a8af179237d3113485d
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-01 21:30:29 +0200

    add Contact page

---
+++ app/javascript/src/pages/Contact/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>patricia.prouvost@outlook.fr</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default Contact;
