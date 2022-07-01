6620ff6176c3ea0a08d7d885e24cbb936f68b44e
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-01 11:14:06 +0200

    generate static_pages controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
+  namespace :api do
+    namespace :v1 do
+      resources :paintings, only: [:index, :show]
+    end
+  end
+
+  get '*path', to: 'static_pages#index', via: :all
 end
