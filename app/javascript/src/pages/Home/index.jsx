a5d5bc66088edeedc208a7d1b6cccfbef7f7967c
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-01 21:31:12 +0200

    create Home page

---
+++ app/javascript/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import NavList from "../../components/NavList";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Patricia Prouvost</h1>
+      <NavList />
+    </div>
+  );
+};
+
+export default Home;
