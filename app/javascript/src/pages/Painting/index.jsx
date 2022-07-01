dbf5352bbfd69ddb2f8ee48ef2b14bb70acdaab5
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-01 21:31:35 +0200

    create Painting page

---
+++ app/javascript/src/pages/Painting/index.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../utils/hooks";
+import { Link } from "react-router-dom";
+
+const Painting = () => {
+  const { id: queryId } = useParams();
+  const painting = useFetch(`/api/v1/paintings/${queryId}`);
+  const element = painting[0];
+
+  return (
+    <div>
+      <h1>{element?.name}</h1>
+      <p>{element?.description}</p>
+      <h6>{element?.format} cm</h6>
+      <h6>{element?.year}</h6>
+      <h5>{element?.price} €</h5>
+      <Link to="/tableaux">Tableaux</Link>
+    </div>
+  );
+};
+
+export default Painting;
