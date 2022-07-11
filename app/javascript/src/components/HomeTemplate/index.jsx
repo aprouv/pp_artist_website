eb539d912bde8f893cb8e4f01147e116c99c3fb2
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-11 18:47:28 +0200

    create different templates for Home and other pages

---
+++ app/javascript/src/components/HomeTemplate/index.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import React from "react";
+
+const HomeTemplate = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  )
+}
+
+export default HomeTemplate
