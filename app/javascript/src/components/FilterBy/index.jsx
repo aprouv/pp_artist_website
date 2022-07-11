f6108d81d7d0e98d6ed1c31aebaec7fe5792d528
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-11 18:50:03 +0200

    update Filter Menu

--- app/javascript/src/components/FilterBy/index.jsx
+++ app/javascript/src/components/FilterBy/index.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const OptionsWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const OptionButton = styled.div`
+  border: 1px solid #adadad;
+  border-radius: 11px;
+  margin: 10px;
+  padding: 7px;
+  &:hover {
+    background: rgba(107, 17, 22, 0.8);
+  }
+`;
+
+const OptionAll = styled.h2`
+  margin-bottom: 0px;
+  text-align: center;
+  font-weight: lighter;
+`;
+
+const StyledLink = styled(Link)`
+  color: #adadad;
+  text-decoration: none;
+  font-size: 14px;
+  &:hover {
+    color: white;
+  }
+`;
 
 const FilterBy = ({ option, paintings }) => {
   const distinctFormats = [...new Set(paintings?.map((x) => x.format))];
@@ -7,45 +38,47 @@ const FilterBy = ({ option, paintings }) => {
 
   return (
     <div>
-      <form>
-        <label>
-          {option == "price" ? (
-            <ul>
-              <li>
-                <Link to={"/tableaux?price=250"}>moins de 250€</Link>
-              </li>
-              <li>
-                <Link to={"/tableaux?price=500"}>250€ - 500€</Link>
-              </li>
-              <li>
-                <Link to={"/tableaux?price=750"}>plus de 750€</Link>
-              </li>
-            </ul>
-          ) : option == "format" ? (
-            distinctFormats.map((format) => (
-              <ul>
-                <li key={format.toString()}>
-                  <Link to={`/tableaux?format=${format}`} format={format}>
-                    {format}
-                  </Link>
-                </li>
-              </ul>
-            ))
-          ) : option == "All" ? (
-            <h2>Tous les tableaux</h2>
-          ) : (
-            distinctYears.map((year) => (
-              <ul>
-                <li key={year.toString()}>
-                  <Link to={`/tableaux?year=${year}`} year={year}>
-                    {year}
-                  </Link>
-                </li>
-              </ul>
-            ))
-          )}
-        </label>
-      </form>
+      {option == "price" ? (
+        <OptionsWrapper>
+          <StyledLink to={"/tableaux?price=500"}>
+            <OptionButton>moins de 500€</OptionButton>
+          </StyledLink>
+
+          <StyledLink to={"/tableaux?price=800"}>
+            <OptionButton>500€ - 800€</OptionButton>
+          </StyledLink>
+
+          <StyledLink to={"/tableaux?price=1000"}>
+            <OptionButton>plus de 1000€</OptionButton>
+          </StyledLink>
+        </OptionsWrapper>
+      ) : option == "format" ? (
+        <OptionsWrapper>
+          {distinctFormats.map((format) => (
+            <StyledLink
+              to={`/tableaux?format=${format}`}
+              key={format.toString()}
+              format={format}
+            >
+              <OptionButton>{format}</OptionButton>
+            </StyledLink>
+          ))}
+        </OptionsWrapper>
+      ) : option == "all" ? (
+        <OptionAll>Tous les tableaux</OptionAll>
+      ) : (
+        <OptionsWrapper>
+          {distinctYears.map((year) => (
+            <StyledLink
+              to={`/tableaux?year=${year}`}
+              key={year.toString()}
+              year={year}
+            >
+              <OptionButton>{year}</OptionButton>
+            </StyledLink>
+          ))}{" "}
+        </OptionsWrapper>
+      )}
     </div>
   );
 };
