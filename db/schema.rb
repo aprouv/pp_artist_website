18a77b3546c9ad3dda15962ea8078351dea04bc2
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-12 14:50:26 +0200

    create db with postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_07_01_082307) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "paintings", force: :cascade do |t|
     t.string "name"
     t.text "description"
