f15543a83132bcbd5d7e72e0dc24a5aab893981d
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-18 17:37:18 +0200

    update tests

--- app/javascript/src/components/Card/index.test.js
+++ app/javascript/src/components/Card/index.test.js
@@ -1,37 +1,9 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
-import { screen, render } from "@testing-library/react";
-
+import { screen, render, fireEvent } from "@testing-library/react";
 import Card from "./";
 
-const paintingsMockedData = [
-  {
-    id: 1,
-    name: "Sortir du cadre",
-    description: "description1",
-    format: "115 x 75",
-    price: 1000,
-    year: 2020,
-  },
-  {
-    id: 2,
-    name: "Innocence et harmonie",
-    description: "description2",
-    format: "115 x 75",
-    price: 1000,
-    year: 2020,
-  },
-  {
-    id: 3,
-    name: "Voyages",
-    description: "description3",
-    format: "80 x 60",
-    price: 650,
-    year: 2019,
-  },
-];
-
-describe("my test", () => {
+describe("the Paintings grid", () => {
   const painting = {
     id: 1,
     name: "Sortir du cadre",
@@ -41,8 +13,7 @@ describe("my test", () => {
     year: 2020,
   };
 
-  it("Should display paintings names", () => {
-    // const history = createMemoryHistory();
+  it("should display the painting's picture", () => {
     const id = painting.id;
     const name = painting.name;
     render(
@@ -50,10 +21,22 @@ describe("my test", () => {
         <Card id={id} name={name} />
       </MemoryRouter>
     );
-    // const image= screen.getByRole(img)
-    expect(screen.getByAltText("Sortir du cadre")).toBeTruthy;
-    // expect(screen.getByText('Filter par')).toBeTruthy()
-    // expect(screen.getByText('Tous les tableaux')).toBeTruthy()
-    // expect(screen.getByText("Si la toile émeut, plaît, interroge, dérange, et si de surcroît parfois elle peut véhiculer une idée, témoigner, secouer des idées, alors le peintre peut être satisfait.")).toBeTruthy()
+    expect(screen.getByAltText(name)).toBeInTheDocument();
+  });
+
+  it("should display the painting's name on hovering", () => {
+    const id = painting.id;
+    const name = painting.name;
+
+    render(
+      <MemoryRouter>
+        <Card id={id} name={name} />
+      </MemoryRouter>
+    );
+    expect(screen.queryByText(name)).toBeNull();
+
+    fireEvent.mouseOver(screen.getByTestId("painting-wrapper"));
+
+    expect(screen.getByText(name)).toBeInTheDocument();
   });
 });
