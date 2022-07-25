33a49ca8ff60ab8894d75e868bf59f94902c4eef
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-25 17:11:17 +0200

    minor format changes

--- app/javascript/src/components/PagesTemplateWhite/index.jsx
+++ app/javascript/src/components/PagesTemplateWhite/index.jsx
@@ -4,7 +4,7 @@ import Header from "../Header";
 
 const PagesTemplate = () => {
   return (
-    <div style={{margin: "0px 20px 20px 20px"}}>
+    <div style={{ margin: "0px 20px 20px 20px" }}>
       <Header />
       <div>
         <Outlet />
