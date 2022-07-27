bafdbfc2744d73a0501c8f3ff2c1eae6e9b74d9e
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-27 19:46:33 +0200

    add model review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_082307) do
+ActiveRecord::Schema.define(version: 2022_07_26_133257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2022_07_01_082307) do
     t.string "format"
     t.integer "price"
     t.integer "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
