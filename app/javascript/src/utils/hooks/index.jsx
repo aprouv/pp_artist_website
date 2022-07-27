2f3baea142134c3fe3ab85f83a2c573411f15046
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-27 19:52:57 +0200

    update hook to make it refetch data when a review is posted

--- app/javascript/src/utils/hooks/index.jsx
+++ app/javascript/src/utils/hooks/index.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 
-const useFetch = (url) => {
+const useFetch = (url, toggle) => {
   const [data, setData] = useState(null);
+
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((err) => console.log(err));
-  }, [url]);
+  }, [url, toggle]);
   return [data];
 };
 
