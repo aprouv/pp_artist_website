93dc8394bf4fc208fa696f85e9d5a1646ca74af9
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-07-29 17:20:34 +0200

    add get & post requests mocks for reviews

--- app/javascript/src/mocks/handlers.js
+++ app/javascript/src/mocks/handlers.js
@@ -1,5 +1,9 @@
 import { rest } from "msw";
-import { paintingsMockedData } from "./data";
+import {
+  paintingsMockedData,
+  reviewMockedData,
+  reviewsMockedData,
+} from "./data";
 
 const handlers = [
   rest.get("http://localhost/api/v1/paintings", (req, res, ctx) => {
@@ -11,6 +15,12 @@ const handlers = [
       ctx.status(200),
       ctx.json(paintingsMockedData.filter((painting) => painting.id == id)[0])
     );
+  }),
+  rest.get("http://localhost/api/v1/reviews", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(reviewsMockedData));
+  }),
+  rest.post("http://localhost:3000/api/v1/reviews", (req, res, ctx) => {
+    return res(ctx.status(201), ctx.json(reviewMockedData));
   }),
 ];
 
