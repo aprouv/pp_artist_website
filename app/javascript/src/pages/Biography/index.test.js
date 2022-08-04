3839ce8f68501f27324ce727c89c48b08d80e6a8
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-08-04 11:24:30 +0200

    add tests

---
+++ app/javascript/src/pages/Biography/index.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import Biography from "./";
+
+describe("the Biography page", () => {
+
+  it("should display the biography text", () => {
+    render(<Biography />);
+    expect(screen.getByText(/Après 44 ans passés à Reims/)).toBeInTheDocument();
+    expect(screen.getByText(/Une journée sans peindre/)).toBeInTheDocument();
+  });
+
+  it("should display the artist's photo", () => {
+    render(<Biography />);
+    expect(screen.getByAltText("Patricia Prouvost")).toBeInTheDocument();
+  });
+});
