8ec0e5fdb4b1c6ceb1fb724cebe110926f554a30
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-08-04 10:29:33 +0200

    change filename

---
+++ app/javascript/src/components/PagesTemplate/index.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import React from "react";
+import Header from "../Header";
+
+const PagesTemplate = () => {
+  return (
+    <div style={{ margin: "0px 20px 20px 20px" }}>
+      <Header />
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default PagesTemplate;
