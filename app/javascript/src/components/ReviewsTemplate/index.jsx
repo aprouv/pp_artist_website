28a040a87938e6a01f601fc057c74a8af5600c5d
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-08-05 15:31:07 +0200

    add footer to template

--- app/javascript/src/components/ReviewsTemplate/index.jsx
+++ app/javascript/src/components/ReviewsTemplate/index.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import React from "react";
 import Header from "../Header";
+import Footer from "../Footer";
 
 const ReviewsTemplate = () => {
   return (
@@ -9,6 +10,7 @@ const ReviewsTemplate = () => {
       <div>
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
