57a2f13604b0a769d95655fa372bdfa339281066
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-08-05 15:32:18 +0200

    add footer to template

--- app/javascript/src/components/PagesTemplate/index.jsx
+++ app/javascript/src/components/PagesTemplate/index.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import React from "react";
 import Header from "../Header";
+import Footer from "../Footer";
 
 const PagesTemplate = () => {
   return (
-    <div style={{ margin: "0px 20px 20px 20px" }}>
+    <div>
       <Header />
-      <div>
+      <div style={{ margin: "0px 20px 20px 20px" }}>
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
